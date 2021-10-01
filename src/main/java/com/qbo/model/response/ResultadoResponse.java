4cebc010e17794af3fecfd0362481c723188ae95
Luis Angel Salvatierra Aquino <luisangel.salvat@gmail.com>
2021-09-30 21:23:40 -0500

    CRUD tabla curso, archivo faltante.

---
+++ src/main/java/com/qbo/model/response/ResultadoResponse.java
@@ -0,0 +1,15 @@
+package com.qbo.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResultadoResponse {
+	private Boolean respuesta;
+	private String mensaje;
+}
