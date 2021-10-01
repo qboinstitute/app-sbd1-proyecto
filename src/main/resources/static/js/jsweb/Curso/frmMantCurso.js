ff4a5f4a1486146d187e5d5abbaa866a11db7ccf
Luis Angel Salvatierra Aquino <luisangel.salvat@gmail.com>
2021-09-30 19:22:58 -0500

    Mantenimiento de curso (javascript)

---
+++ src/main/resources/static/js/jsweb/Curso/frmMantCurso.js
@@ -0,0 +1,18 @@
+$(document).on("click", "#btnagregarcurso", function(){
+	$("#modalcurso").modal("show");
+	
+});
+
+$(document).on("click", ".btnactualizarcurso", function(){
+	$("#modalcurso").modal("show");
+});
+
+$(document).on("click", "#btnregistrarcurso", function(){
+	
+});
+
+
+
+$(document).on("click", ".btneliminarcurso", function(){
+	$("#modaleliminarcurso").modal("show");
+});
